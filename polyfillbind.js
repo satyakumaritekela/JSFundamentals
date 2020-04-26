85c448c24726f9227fbb7fb7f09c6cc57ba8f8ae
Satya Kumar Itekela <st798799@dal.ca>
2020-04-26 20:49:09 -0300

    updating polyfillbind.js

--- polyfillbind.js
+++ polyfillbind.js
@@ -2,10 +2,17 @@ let name = {
     firName: 'Satya Kumar'
 }
 
-let printName = function(greetings) {
-    console.log(greetings + this.firName)
+let printName = function(greetings, moregreetings) {
+    console.log(greetings + this.firName + moregreetings)
 }
 
-function.prototype.mybind = function () {
-    
+Function.prototype.mybind = function(...args) {
+    let obj = this;
+    return function (...args2) {
+        obj.apply(args[0], [...args.slice(1),...args2]);
+    }    
 }
+
+let pName = printName.mybind(name, "hello");
+
+pName("hhhhh");
